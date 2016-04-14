6ebc3616f4c47562ddea2828d0f2f96fc7aa6076
Maurice Karg <mk@k2dev.de>
2016-04-14 10:40:26 +0200

    add zend-mvc < 2.7 support

--- Module.php
+++ Module.php
@@ -5,31 +5,40 @@ namespace TccSkippableSegment;
 use TccSkippableSegment\Mvc\Router\Http\SkippableSegment;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\Mvc\Router\RouteInvokableFactory;
 
 class Module implements ConfigProviderInterface, AutoloaderProviderInterface
 {
     public function getConfig()
     {
-        return [
-            'route_manager' => [
-                'invokables' => [],
+        if (class_exists('\Zend\Mvc\Router\RouteInvokableFactory')) {
+            // new version for zend-mvc >= 2.7
+            $route_manager = [
                 'aliases' => [
                     'SkippableSegment' => SkippableSegment::class,
                 ],
                 'factories' => [
-                    SkippableSegment::class => RouteInvokableFactory::class,
+                    SkippableSegment::class => \Zend\Mvc\Router\RouteInvokableFactory::class,
                 ]
-            ],
+            ];
+        } else {
+            // old zend-mvc version
+            $route_manager = [
+                'invokables' => [
+                    'SkippableSegment' => SkippableSegment::class,
+                ],
+            ];
+        }
+        return [
+            'route_manager' => $route_manager
         ];
     }
 
     public function getAutoloaderConfig()
     {
-        return [
-            'Zend\Loader\ClassMapAutoloader' => [
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
                 __DIR__ . '/autoload_classmap.php',
-            ],
-        ];
+            ),
+        );
     }
 }
